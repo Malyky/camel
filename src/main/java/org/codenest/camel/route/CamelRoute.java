34f851d216ec515f3ef72e39910e4b213d4d0844
Martin Ly-Ky <martin.ly-ky@gmx.de>
2023-02-08 16:30:16 +0100

    GettingStarted

--- src/main/java/org/codenest/camel/route/CamelRoute.java
+++ src/main/java/org/codenest/camel/route/CamelRoute.java
@@ -3,14 +3,17 @@ package org.codenest.camel.route;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.file;
 import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.timer;
 
 @Component
 public class CamelRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from(timer("mytimer").period(1000))
+        from(timer("mytimer").period(10000))
                 .log("test")
+                .setBody(constant("setAnythingToBody"))
+                .to(file("outputPath").fileName("thisWasGenerated.txt"))
                 ;
     }
 }
